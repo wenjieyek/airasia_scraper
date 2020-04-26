6e55af499a7299db60b7e8f2d0191e888e8e851a
wenjieyek <dx3152@hotmail.com>
2020-04-27 02:04:51 +0800

    add spec

--- lib/airasia_scraper.rb
+++ lib/airasia_scraper.rb
@@ -3,11 +3,11 @@ require "nokogiri"
 require "airasia_scraper/version"
 
 module AirasiaScraper
-  class Client 
+  class Client
     def initialize(depart_date, from, to)
-      @depart_date = depart_date.upcase
-      @from = from
-      @to = to
+      @depart_date = depart_date
+      @from = from.upcase
+      @to = to.upcase
     end
 
     def flight_data
@@ -17,7 +17,7 @@ module AirasiaScraper
         flight_message = flight_page_html.content.split(" ")
         depart_time = flight_message[1].delete(".")
         arrive_time = flight_message[3].delete(".")
-        fare = flight_message.last
+        fare = flight_message.select { |n| n.include? "MYR" }.first.split("MYR")[0]
         return_flight_data_hash(depart_time, arrive_time, fare)
       end
     end
@@ -25,13 +25,17 @@ module AirasiaScraper
     private
     def get_flight_page_html
       browser = Watir::Browser.new
-      browser.goto "https://www.airasia.com/select/en/gb/#{@from}/#{@to}/#{@depart_date}/N/1/0/0/O/N/MYR/ST?key=ba68bcc7-87d7-11ea-8cc4-c9875338e7591587917120.34"
+      browser.goto airasia_link
       sleep 30
       Nokogiri::HTML(browser.html)
     end
 
     def return_flight_data_hash(depart_time, arrive_time, fare)
-      { depart_time: depart_time, arrive_time: arrive_time, fare: fare }
+      { depart_time: depart_time, arrive_time: arrive_time, fare: fare, link: airasia_link }
+    end
+
+    def airasia_link
+      "https://www.airasia.com/select/en/gb/#{@from}/#{@to}/#{@depart_date}/N/1/0/0/O/N/MYR/ST?key=ba68bcc7-87d7-11ea-8cc4-c9875338e7591587917120.34"
     end
   end
 end
--- spec/airasia_scraper_spec.rb
+++ spec/airasia_scraper_spec.rb
@@ -1,9 +1,17 @@
+require "spec_helper"
+
 RSpec.describe AirasiaScraper do
   it "has a version number" do
     expect(AirasiaScraper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns correct flight data hash" do
+    c = AirasiaScraper::Client.new("2021-01-02", "KUL", "JHB")
+    flight_data_hash = c.flight_data
+
+    expect(c.flight_data.first[:depart_time]).to eq("07:15")
+    expect(c.flight_data.first[:arrive_time]).to eq("08:15")
+    expect(c.flight_data.first[:fare]).to eq("84.30")
+    expect(c.flight_data.first[:link]).to eq("https://www.airasia.com/select/en/gb/KUL/JHB/2021-01-02/N/1/0/0/O/N/MYR/ST?key=ba68bcc7-87d7-11ea-8cc4-c9875338e7591587917120.34")
   end
 end
